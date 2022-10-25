b6b28b2806d6b7c9aaf1c792e3e93ea833c8ad72
Anton Bochkovsky <98012691+firehawk89@users.noreply.github.com>
2022-10-25 13:31:30 +0300

    Revert "Revert "Added basic file structure, created header block.""

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log("Hello, World!");
+const menu = document.querySelector(".menu");
+const social = document.querySelector(".social");
+const burger = document.querySelector(".burger__body");
+const options = document.querySelectorAll(".options__item");
+const header = document.querySelector("header");
+
+/* burger menu */
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+  document.body.classList.toggle("lock");
+});
+
+menu.querySelectorAll(".menu__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
+    document.body.classList.remove("lock");
+  });
+});
+
+/* the appearence of contact popup and scroll to top buttons */
+const headerHeight = header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  for (let option of options) {
+    if (window.pageYOffset > headerHeight) {
+      option.classList.add("active");
+    } else {
+      option.classList.remove("active");
+    }
+  }
+});
+
+/* smooth page transition */
+const anchors = document.querySelectorAll("a[href*='#']");
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
